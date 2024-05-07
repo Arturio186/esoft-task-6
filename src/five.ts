30eba70ab74eaead95c4c743e43ea65f5fb994b7
Arturio186 <bain.romanoff@mail.ru>
2024-05-07 23:20:26 +0500

    five (need fix)

---
+++ src/five.ts
@@ -0,0 +1,34 @@
+//---------------------------------------------------------------------------------
+//Задание 5: Работа с Indexed Access Types и Mapped Types
+//Цель: Создать утилиты для работы с объектами и их ключами.
+
+// Определите интерфейс для пользователя
+interface User2 {
+  id: number;
+  name: string;
+  email: string;
+  age: number;
+}
+
+// Используйте Indexed Access Types для получения типа поля name из User
+type UserNameType = User2["name"];
+
+// Создайте Mapped Type, который преобразует все поля интерфейса User в boolean. Можно воспользовать конструкцией Key in keyof
+type UserFieldsToBoolean = {
+  [Key in keyof User2]: boolean;
+};
+
+// Реализуйте функцию, которая принимает ключи интерфейса User и возвращает их типы
+function getUserFieldType(key: keyof User2) {
+  // Верните тип ключа
+  // WTFFFFF????????
+  // ВЕРНУСЬ ПОЗЖЕ((((((
+  const helper: User2 = { id: 0, name: "", email: "", age: 0 };
+
+  return typeof helper[key]
+}
+
+// Используйте эту функцию для получения типа поля 'age' и 'name'
+const ageType = getUserFieldType("age");
+const nameType = getUserFieldType("name");
+//---------------------------------------------------------------------------------
